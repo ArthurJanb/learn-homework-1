a3405ef25e736df6f601fb9e8be91b3c1f59a626
Arthur <arthur.janb@gmail.com>
2021-05-26 22:49:20 +0300

    Homework #1

--- 7_exception2.py
+++ 7_exception2.py
@@ -1,28 +1,38 @@
-"""
+#-------------------
+#Домашнее задание №1
+#-------------------
 
-Домашнее задание №1
+# Исключения: приведение типов
 
-Исключения: приведение типов
+#* Перепишите функцию discounted(price, discount, max_discount=20)
+#  из урока про функции так, чтобы она перехватывала исключения,
+#  когда переданы некорректные аргументы.
+#* Первые два нужно приводить к вещественному числу при помощи float(),
+#  а третий - к целому при помощи int() и перехватывать исключения
+#  ValueError и TypeError, если приведение типов не сработало.
 
-* Перепишите функцию discounted(price, discount, max_discount=20)
-  из урока про функции так, чтобы она перехватывала исключения,
-  когда переданы некорректные аргументы.
-* Первые два нужно приводить к вещественному числу при помощи float(),
-  а третий - к целому при помощи int() и перехватывать исключения
-  ValueError и TypeError, если приведение типов не сработало.
-    
-"""
+def discounted(price, discount, max_discount=20):
 
-def discounted(price, discount, max_discount=20)
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
-if __name__ == "__main__":
-    print(discounted(100, 2))
-    print(discounted(100, "3"))
-    print(discounted("100", "4.5"))
-    print(discounted("five", 5))
-    print(discounted("сто", "десять"))
-    print(discounted(100.0, 5, "10"))
+    try:                                                                        # конструкция try - except позволяет выловить исключение
+        price = abs(float(price))                                               # в блоке try выполняется инструкция, в которой может быть исключение
+        discount = abs(float(discount))
+        max_discount = abs(int(max_discount))
+    except (TypeError, ValueError):                                             # в блоке except указаны возможные исключения, если исключение определено
+        return "Ошибка. Аргументам функции переданы неправильные значения"      # выводим ошибку
+
+    if max_discount > 99:
+        raise ValueError("Слишком большая максимальная скидка")
+    if discount >= max_discount:
+        price_with_discount = price
+    else:
+        price_with_discount = price - (price * discount/100)
+    return price_with_discount
+
+if __name__ == '__main__':
+
+    print(discounted(50, 3))       
+    print(discounted(70, '6'))
+    print(discounted('40', '3.3'))
+    print(discounted('one', 13))
+    print(discounted('twenty', 'one'))
+    print(discounted(65.3, 12, '20'))
--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -1,57 +1,88 @@
-"""
-Домашнее задание №1
+#-------------------
+#Домашнее задание №1
+#-------------------
 
-Использование библиотек: ephem
+# Использование библиотек: ephem
 
-* Установите модуль ephem
-* Добавьте в бота команду /planet, которая будет принимать на вход
-  название планеты на английском, например /planet Mars
-* В функции-обработчике команды из update.message.text получите
-  название планеты (подсказка: используйте .split())
-* При помощи условного оператора if и ephem.constellation научите
-  бота отвечать, в каком созвездии сегодня находится планета.
+#* Установите модуль ephem
+#* Добавьте в бота команду /planet, которая будет принимать на вход
+#  название планеты на английском, например /planet Mars
+#* В функции-обработчике команды из update.message.text получите
+#  название планеты (подсказка: используйте .split())
+#* При помощи условного оператора if и ephem.constellation научите
+#  бота отвечать, в каком созвездии сегодня находится планета.
 
-"""
-import logging
+# Задание помещено в репозиторий "Mybot"
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging, ephem # Импортирование модуля регистрации и модуля астрономических величин
 
-logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters # Импортируем модули отвечающие за ком-цию с Telegram, обработку сообщенийб обработку команд
+
+from datetime import date, datetime # Импортируем модули времени, что бы определять дату и время
+
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s', # журнал для записи отчета о работе бота
                     level=logging.INFO,
                     filename='bot.log')
 
 
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
-    }
-}
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080', 'urllib3_proxy_kwargs': 
+        {'username': 'learn','password': 'python'}}
 
 
-def greet_user(update, context):
-    text = 'Вызван /start'
-    print(text)
-    update.message.reply_text(text)
+def greet_user(update, context): # функция которая приветствует пользователя
+    print('Вызван /start')      # вызывается если пользователь вводит \start 
+    update.message.reply_text() # - отвечает пользователю отправляя сообщение через telegram
+    
+
+def talk_to_me(update, context):    # функция которая отвечает пользователю эхом
+    user_text = update.message.text 
+    print(user_text)                
+    update.message.reply_text(user_text) 
 
 
-def talk_to_me(update, context):
-    user_text = update.message.text
-    print(user_text)
-    update.message.reply_text(text)
+def ask_planet(update, context): # функция которая должна отвечать пользователю если он ввел название планеты
+    user_text = update.message.text   # получаем от пользователя сообщение
+    user_text = user_text.split(' ')  # делаем список из сообщения пользователя
+
+    if user_text[0] == "/planet":  #Перебираем планеты
+        date = datetime.datetime(now)  # обозначили что нужна сегодняшняя дата
+        if user_text == 'Mars':     # если тест от пользователя приравнен к слову Mars
+            mars = ephem.Mars(date)  # то в переменную mars кладется расположение планеты сегодня
+            update.message.reply_text(ephem.constellation(mars)) # выводится сообщение пользователю о созвездии
+        if user_text == 'Venus':
+            venus = ephem.Venus(date)
+            update.message.reply_text(ephem.constellation(venus))
+        if user_text == 'Mercury':
+            mercury = ephem.Mercury(date)
+            update.message.reply_text(ephem.constellation(mercury))
+        if user_text == 'Jupiter':
+            jupiter = ephem.Jupiter(date)
+            update.message.reply_text(ephem.constellation(jupiter))
+        if user_text == 'Saturn':
+            saturn = ephem.Saturn(date)
+            update.message.reply_text(ephem.constellation(saturn))
+        if user_text == 'Uranus':
+            uranus = ephem.Uranus(date)
+            update.message.reply_text(ephem.constellation(uranus))
+        if user_text == 'Neptune':
+            neptune = ephem.Neptune(date)
+            update.message.reply_text(ephem.constellation(neptune))
+        if user_text == 'Pluto':
+            pluto = ephem.Pluto(date)
+            update.message.reply_text(ephem.constellation(pluto))
 
 
-def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
+def main():                            # 
+    mybot = Updater("1811105015:AAGtCAWBsYaMg6tFkJq8NqG5CFHljIZLfOs", request_kwargs=PROXY, use_context=True) 
 
-    dp = mybot.dispatcher
+    dp = mybot.dispatcher # функция подключения к telegram
     dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler("planet", ask_planet))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me)) 
 
     mybot.start_polling()
     mybot.idle()
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
